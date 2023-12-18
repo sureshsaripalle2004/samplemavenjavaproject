99d2cbc0313b9befbb1c8ac94583ce7c5024289c
SURESH <sureshsaripalle@gmail.com>
2023-12-18 12:18:36 +0530

    Demo for team

---
+++ src/main/java/com/app/demo/samplemavenjavaproject/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.samplemavenjavaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "creating a CI/CD pipeline using jenkins for a sample maven java project" );
+    }
+}
